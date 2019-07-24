77a6dafa63513fa05a04bbdd4e468b5f46e5a6a3
zylzzz <1954047590@qq.com>
2019-07-24 22:04:25 +0800

    little thing,don't care

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,26 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
     <comp></comp>
-    <router-link to="/">点我</router-link>
-    <router-link to="/login">点我另一个</router-link>
+    <router-link to="/">点我</router-link><br/>
+    <router-link to="/login">切换</router-link>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 import comp from './components/comp.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld,
     comp
-  }
+  },
+  data:function(){return {msg:"hello"}}
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
